1b82407aadf899c1105e1f66b628f3f9c9cab466
oMoFoLaYaN <nathanadeosun2003@gmail.com>
2023-03-31 12:43:40 +0100

    through

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -15,30 +15,30 @@ const Menu = () => {
       <div className={layout.row}>
         <div className="lg:w-full lg:basis-auto mx-auto flex flex-row justify-center">
         {menu.filters.map((filter) => (
-          <ul key={filter.id} id="menu-flters" className='text-center mx-0 my-0 p-0'>
-            <li data-filter={filter.id} className="filter-active capitalize cursor-pointer inline-block text-base font-medium leading-none transition-all ease-in-out delay-300 mb-2.5 pt-2 pb-2.5 px-3"><a>{filter.name}</a></li> 
+          <ul key={filter.id} id="menu-flters" className='text-right xs:text-center mx-0 my-0 p-0'>
+            <li data-filter={filter.id} className="filter-active capitalize cursor-pointer inline-block xs:text-base text-xs font-medium leading-none transition-all ease-in-out delay-300 mb-2.5 pt-2 pb-2.5 px-3"><a>{filter.name}</a></li> 
           </ul>
         ))}
         </div>
       </div>
       <div className={layout.row}>
         {menu.menus.map((menu) => (
-        <div key={menu.name} className="w-full lg:w-5/12 my-8 lg:mx-8 px-10 py-3 h-44 bg-fola-100 dark:bg-fola-950 rounded-xl">
+        <div key={menu.name} className="w-full lg:w-5/12 my-8 lg:mx-8 px-5 py-3 h-fit bg-fola-100 dark:bg-fola-950 rounded-xl">
           <div className='flex flex-row justify-between'>
           <img src={menu.img} className="menu-img w-24 relative -top-10 -right-4 shadow-2xl rounded-full shadow-fola-950 dark:shadow-fola-900 " alt={menu.name}/>
           <i className='bi bi-heart-fill p-3 text-xl text-fola-600 dark:text-fola-500'></i>
           </div>
           <div className="menu-content flex flex-row relative -top-5 justify-between">
-            <a href="#" className='font-bold capitalize text-lg font-display relative text-fola-950 dark:text-fola-100'>{menu.name}</a>
-            <span className='font-semibold text-fola-900 dark:text-fola-200'>${menu.price}</span>
+            <a href="#" className='font-bold capitalize xs:text-xl text-base font-display relative text-fola-950 dark:text-fola-100'>{menu.name}</a>
+            <span className='font-semibold text-fola-900 dark:text-fola-200 text-sm xs:text-base'>${menu.price}</span>
           </div>
-          <p className="menu-ingredients text-xs relative text-fola-800 dark:text-fola-300 -top-5">{menu.ingredients}</p>
+          <p className="menu-ingredients xs:text-xs text-[0.6rem] relative text-fola-800 dark:text-fola-300 -top-5 capitalize">{menu.ingredients}</p>
           <div className='flex flex-row justify-between'>
-          <div className='flex flex-row relative -top-4'>
-          <i className='bi bi-star text-md text-fola-700 dark:text-fola-500'/>
-          <p className='relative ml-1 lg:ml-3 text-base text-fola-900 dark:text-fola-300'>{menu.rating}</p>
+          <div className='flex flex-row relative -bottom-4'>
+          <i className='bi bi-star sm:text-md text-sm text-fola-700 dark:text-fola-500'/>
+          <p className='relative ml-1 lg:ml-3 sm:text-md text-sm text-fola-900 dark:text-fola-300'>{menu.rating}</p>
           </div>
-          <a href="#" className='px-4 py-1 bg-fola-400 text-fola-900 font-semibold rounded-b-3xl relative top-2'>Eat This</a>
+          <a href="#" className='px-4 py-1 bg-fola-400 text-fola-900 font-semibold rounded-b-3xl relative -bottom-7 rounded-t-sm border-t-fola-950 dark:border-t-fola-150'>Eat This</a>
           </div>
           
         </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,19 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
+    screens: {
+      'xs': '340px',
+      
+      'sm': '640px',
+
+      'md': '768px',
+
+      'lg': '1024px',
+
+      'xl': '1280px',
+
+      '2xl': '1536px',
+    },
     extend: {
       fontFamily: {
         'display': ['Fira Mono, monospace'],
